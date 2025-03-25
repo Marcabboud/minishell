75e87e1126a635a947fcc611848f6110d644257b
Marcabboud <supermarcabb@gmail.com>
2025-03-25 16:57:47 +0100

    echo

--- Builtins/builtins.c
+++ Builtins/builtins.c
@@ -34,26 +34,23 @@ int	ft_exit_validate(t_global *g, t_cmd *cmd)
     	g->exit_val = 1;
     	return (1);
 }
-static	t_state	ft_echo(t_global *g, t_cmd *cmd)
+static t_state	ft_echo(t_global *g, t_cmd *cmd)
 {
-	int	i = 1;
-    bool	new_line = true;
+	int	i;
 
-    	if (cmd->args[1] && ft_strcmp(cmd->args[1], "-n"))
-    {
-	new_line = false;
-        i++;
-    }
-    while (cmd->args[i])
-    {
-	write(1, cmd->args[i], 	ft_strlen(cmd->args[i]));
-        	if (cmd->args[++i])
-            	write(1, " ", 1);
-    }
-    	if (new_line)
-        	write(1, "\n", 1);
-    	g->exit_val = 0;
-    	return (VALID);
+	i = 1;
+	if (cmd->args[1] && ft_strcmp(cmd->args[1], "-n"))
+		i++;
+	while (cmd->args[i] != NULL)
+	{
+		write(1, cmd->args[i], ft_strlen(cmd->args[i]));
+		if (cmd->args[i + 1] != NULL)
+			write(1, " ", 1);
+		i++;
+	}
+	if (cmd->args[1] && ft_strcmp(cmd->args[1], "-n"))
+		return (g->exit_val = 0, VALID);
+	return (write(1, "\n", 1), g->exit_val = 0, VALID);
 }
 static	t_state	gest_env(t_global *g, char **arr)
 {
