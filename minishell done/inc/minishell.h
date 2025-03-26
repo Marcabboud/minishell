72e83f6968961579a73f76ef372e0bc3b44f8417
Marcabboud <supermarcabb@gmail.com>
2025-03-26 14:05:16 +0100

    yes

--- minishell done/inc/minishell.h
+++ minishell done/inc/minishell.h
@@ -50,9 +50,9 @@ void	ft_free_node(t_env *node);
 
 /*BUILTINS*/
 t_state	gest_builtins(t_global *g, t_cmd *cmd);
-int		ft_exit(t_global *g, t_cmd *cmd, bool print);
+int		ft_exit(t_global *g, t_cmd *cmd, bool message);
 bool	ft_export(t_global *g, char **str, bool multiples);
-bool	ft_cd(t_global *g, char **arr);
+bool	ft_cd(t_global *g, char **args);
 void	ft_unset(t_global *g, char **str);
 t_state	is_format_export(char *str);
 
